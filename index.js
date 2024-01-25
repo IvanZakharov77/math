13285d406902b511eaaa1806a09b2c5541392c3d
IvanZakharov77 <ivan.zzaharov@gmail.com>
2024-01-25 12:09:27 +0200

    mobip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ip3 = '46.150.10.154';
 const ip1 = '2a02:908:d11:3520::cccb';
 const ip2 = '31.202.123.78';
+const ip4 = '88.154.56.58';
 // Создаем новый объект XMLHttpRequest
 var xhr = new XMLHttpRequest();
 let ipAddress;
@@ -53,6 +54,9 @@ function startGame() {
     showImage(optimus1);
     audio.play();
   } else if (ipAddress === ip2) {
+    showImage(optimus1);
+    audioL.play();
+  } else if (ipAddress === ip4) {
     showImage(optimus1);
     audioL.play();
   } else {
